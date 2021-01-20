d2209d9f5781fe66794498f617ed5889e5e26f11
Richárd Váradi <ricsi19981007@gmail.com>
2021-01-20 08:25:35 +0100

    Separate the call generator from the commands strings
    It is needed to make the code more readable and more general.

--- bin/call_gen.py
+++ bin/call_gen.py
@@ -1,57 +1,57 @@
 from utils import send, ffmpeg
+import commands
 import sdp_transform
 import time
+import json
 
-# Generate an Answer Dict 
-def generate_answer(args, call_id, label, from_tag, to_tag, port):
-    data = {}
-    data["ICE"] = "remove"
-    data["call-id"] = str(call_id)
-    data["command"] = "answer"
-    data["from-tag"] = str(from_tag)
-    data["label"] = str(label)
-    data["sdp"] = "v=0\r\no=- 1607446271 1 IN IP4 " + args.sdpaddr + "\r\ns=tester\r\nt=0 0\r\nm=audio " + str(port) + " RTP/AVP 0\r\nc=IN IP4 " + args.sdpaddr + "\r\na=sendrecv\r\na=rtcp:" + str(port + 1)
-    data["to-tag"] = str(to_tag)
-    return data
-
-# Generate an Offer Dict
-def generate_offer(args, call_id, label, from_tag, port):
-    data = {}
-    data["ICE"] = "remove"
-    data["call-id"] = str(call_id)
-    data["command"] = "offer"
-    data["from-tag"] = str(from_tag)
-    data["label"] = str(label)
-    data["sdp"] = "v=0\r\no=- 1607444729 1 IN IP4 " + args.sdpaddr + "\r\ns=tester\r\nt=0 0\r\nm=audio " + str(port) + " RTP/AVP 0\r\nc=IN IP4 " + args.sdpaddr + "\r\na=sendrecv\r\na=rtcp:" + str(port + 1)
-    return data
-
-# Start a certain number of calls.
-# All calls have different ID and port pair
 def generate_calls(args, cnt):
+    ''' Generate a given number of calls.
+
+    The first call will have these ports: offer - 3002 and answer - 
+    3004 and it will increase always by two. This is important to debug
+    if somethin went wrong. 
+
+    It will use the ffmpeg command to create subprocesses which runs 
+    almost side-by-side. 
+
+    Watch out for the cnt because it can produce a huge load cause the 
+    ffmpeg memory usage. 
+
+    Args:
+      args: An object with the user settings.
+      cnt: Number of concurrent calls. 
+    '''
+
     start_port = 3000
-    offers = []
-    answers = []
+    offers = []; answers = []
+    
     for _ in range(cnt):
+        # Offer
         start_port += 2
-        # Send an offer 
-        offer = send(args, generate_offer(args, str(start_port) + "-" + str(start_port + 2), "caller" + str(start_port), 
-            "from-tag" + str(start_port), start_port), 
+        sdp_offer = json.loads(
+            commands.offer("remove", str(start_port) + "-" + str(start_port + 2), 
+            "from-tag" + str(start_port), "caller" + str(start_port), 
             args.sdpaddr, start_port)
+        )
+        offer = send(args, sdp_offer, args.sdpaddr, start_port)
+
+        # Answer
         start_port += 2
-        # Send an answer
-        answer = send(args, generate_answer(args, str(start_port - 2) + "-" + str(start_port), "callee" + str(start_port), 
-            "from-tag" + str(start_port - 2), "to-tag" + str(start_port), start_port), 
-            args.sdpaddr, start_port)
-        # Parse the responses 
+        sdp_answer = json.loads(
+            commands.answer("remove", str(start_port - 2) + "-" + str(start_port),
+            "from-tag" + str(start_port - 2), "to-tag" + str(start_port), 
+            "callee" + str(start_port), args.sdpaddr, start_port)
+        )
+        answer = send(args, sdp_answer,args.sdpaddr, start_port)
+        
         parsed_offer = sdp_transform.parse(offer.get('sdp'))
         parsed_answer = sdp_transform.parse(answer.get('sdp'))
-        # Get the generated ports
+        
         offer_port = parsed_offer.get('media')[0].get('port')
         answer_port = parsed_answer.get('media')[0].get('port')
-        # Generate addresses to send traffic to them 
-        offers.append("rtp://" + args.addr + ":" + str(offer_port) + "?localrtpport=" + str(start_port - 2))
-        answers.append("rtp://" + args.addr + ":" + str(answer_port) + "?localrtpport=" + str(start_port))
-    # Wait a second to close every port what the for loop is opened 
+        
+        offers.append(f'rtp://{args.addr}:{str(offer_port)}?localrtpport={str(start_port - 2)}')
+        answers.append(f'rtp://{args.addr}:{str(answer_port)}?localrtpport={str(start_port)}')
+
     time.sleep(1)
-    # Start the streams 
     ffmpeg(args, cnt, offers, answers)
---
+++ bin/commands.py
@@ -0,0 +1,282 @@
+
+def ping():
+    return f'''
+    {{
+        "command": "ping"
+    }}
+    '''
+
+def offer(ice, call_id, from_tag, label, sdpaddr, port):
+    # TODO: Somehow pass an argument that contains all the unnecessary keys 
+    # and only write it to a string if it is defined, otherwise not.
+    return fr'''
+    {{
+        "ICE": "{ice}",
+        "call-id": "{str(call_id)}",
+        "command": "offer",
+        "from-tag": "{str(from_tag)}",
+        "label": "{str(label)}",
+        "sdp": "v=0\r\no=- 1607444729 1 IN IP4 {sdpaddr}\r\ns=tester\r\nt=0 0\r\nm=audio {str(port)} RTP/AVP 0\r\nc=IN IP4 {sdpaddr}\r\na=sendrecv\r\na=rtcp: {str(port + 1)}"
+    }}
+    '''
+
+def answer(ice, call_id, from_tag, to_tag, label, sdpaddr, port):
+    # TODO: Somehow pass an argument that contains all the unnecessary keys 
+    # and only write it to a string if it is defined, otherwise not.
+    return fr'''
+    {{
+        "ICE": "{ice}",
+        "call-id": "{str(call_id)}",
+        "command": "answer",
+        "from-tag": "{str(from_tag)}",
+        "label": "{str(label)}",
+        "sdp": "v=0\r\no=- 1607446271 1 IN IP4 {sdpaddr}\r\ns=tester\r\nt=0 0\r\nm=audio {str(port)} RTP/AVP 0\r\nc=IN IP4 {sdpaddr}\r\na=sendrecv\r\na=rtcp: {str(port + 1)}",
+        "to-tag": "{str(to_tag)}"
+    }}
+    '''
+
+def delete(call_id, from_tag):
+    # TODO: Add conditionally: to-tag, via-branch, flags, delete delay
+    return f'''
+    {{
+        "command": "delete",
+        "call-id": "{str(call_id)}",
+        "from-tag": "{str(from_tag)}"
+    }}
+    '''
+
+def list_calls(limit = 32):
+    ''' Get a list of call-ids
+
+    Be careful with the limit. To high could cause error. The default 
+    value is 32. 
+    '''
+
+    return f'''
+    {{
+        "command": "list",
+        "limit": "{str(limit)}"
+    }}
+    '''
+
+def query(call_id, from_tag = '-1', to_tag = '-1'):
+    ''' Query data about a call by call_id.
+
+    It will contain data about packets, protcols, etc. 
+    '''
+
+    return f'''
+    {{
+        "command": "query",
+        "call-id:" "{str(call_id)}",
+        {'"from-tag": "' + str(from_tag) + '",' if from_tag != '-1' else ''}
+        {'"to-tag": "' + str(to_tag) + '",' if to_tag != '-1' else ''}
+    }}
+    '''
+
+def start_recording(call_id, from_tag = '-1', to_tag = '-1', via_branch = '-1'):
+    ''' Enables call recording for the call. 
+
+    Either for the entire call or for only the specified call leg. 
+    Currently rtpengine always enables recording for the entire call and
+    does not support recording only individual call legs, therefore all
+    keys other than call-id are currently ignored.
+    '''
+
+    return f'''
+    {{
+        "command": "start recording",
+        "call-id": "{str(call_id)}",
+        {'"from-tag": "' + str(from_tag) + '",' if from_tag != '-1' else ''}
+        {'"to-tag": "' + str(to_tag) + '",' if to_tag != '-1' else ''}
+        {'"via-branch": "' + str(via_branch) + '",' if via_branch != '-1' else ''}
+    }}
+    '''
+
+def stop_recording(call_id):
+    ''' Disables call recording for the call. This can be sent during a
+    call to immediately stop recording it.
+    '''
+
+    return f'''
+    {{
+        "command": "stop recording"
+    }}
+    '''
+
+def block_dtmf(call_id, from_tag = '-1', address = '-1', label = '-1'):
+    ''' Disable DTMF events. (RFC 4733)
+    
+    If only the call_id is presence will block DTMF for the entire call
+    but, it can be blocked for individual participants by from_tag,
+    address (SDP media address) or label. 
+    '''
+
+    return f'''
+    {{
+        "command": "block dtmf",
+        "call-id": "{str(call_id)}",
+        {'"from-tag": "' + str(from_tag) + '",' if from_tag != '-1' else ''}
+        {'"address": "' + str(address) + '",' if address != '-1' else ''}
+        {'"label": "' + str(label) + '",' if label != '-1' else ''}
+    }}
+    '''
+
+def unblock_dtmf(call_id, all = False):
+    ''' Unblock DTMF. 
+
+    Unblocking packets for the entire call (i.e. only call-id is given)
+    does not automatically unblock packets for participants which had 
+    their packets blocked directionally, unless the string all is 
+    included in the flags section of the message.
+    '''
+
+    return f'''
+    {{
+        "command": "unblock dtmf",
+        "call-id": "{str(call_id)}",
+        {'"flags": ["all"],' if all else ''}
+    }}
+    '''
+
+def block_media(call_id, from_tag = '-1', address = '-1', label = '-1'):
+    ''' Block media packets. 
+
+    DTMF packets can still pass through when media blocking is enabled.
+    Media packets can be blocked fo an entire call, or directionally for
+    individual participants.
+
+    '''
+    return f'''
+    {{
+        "command": "block media",
+        "call-id": "{str(call_id)}",
+        {'"from-tag": "' + str(from_tag) + '",' if from_tag != '-1' else ''}
+        {'"address": "' + str(address) + '",' if address != '-1' else ''}
+        {'"label": "' + str(label) + '",' if label != '-1' else ''}
+    }}
+    '''
+
+def unblock_media(call_id, all = False):
+    ''' Unblock media packets. 
+    
+    Works like unblock_dtmf(...). 
+    '''
+
+    return f'''
+    {{
+        "command": "unblock media",
+        "call-id": "{str(call_id)}",
+        {'"flags": ["all"],' if all else ''}
+    }}
+    '''
+
+def start_forwarding(call_id, from_tag = '-1', address = '-1', label = '-1'):
+    ''' Forward PCM via TCP/TLS.
+
+    These messages control the recording daemon's mechanism to forward
+    PCM via TCP/TLS. Unlike the call recording mechanism, forwarding can
+    be enabled for individual participants (directionally) only.
+    '''
+
+    return f'''
+    {{
+        "command": "start forwarding",
+        "call-id": "{str(call_id)}",
+        {'"from-tag": "' + str(from_tag) + '",' if from_tag != '-1' else ''}
+        {'"address": "' + str(address) + '",' if address != '-1' else ''}
+        {'"label": "' + str(label) + '",' if label != '-1' else ''}
+    }}
+    '''
+
+def stop_forwarding(call_id, all = False):
+    ''' Stop forwarding.
+
+    Works like unblock_dtmf(...).
+    '''
+
+    return f'''
+    {{
+        "command": "stop forwarding",
+        "call-id": "{str(call_id)}",
+        {'"flags": ["all"],' if all else ''}
+    }}
+    '''
+
+def play_media(call_id, file, from_tag = '-1', address = '-1', label = '-1',
+                all = False):
+    ''' Starts playback of provided media file. 
+
+    Important: Only available if the rtpengine was compiled with 
+      transcoding support. 
+
+    You can play media only participants by identify them with these 
+    keys: from-tag, address, label. Or you can play media every 
+    participants by set flag to all. 
+
+    The played media could be anything what ffmpeg supports. 
+    '''
+
+    # TODO: Add all the possible file provider keys. 
+    return f'''
+    {{
+        "command": "play media",
+        "call-id": "{str(call_id)}",
+        "file": "{str(file)}",
+        {'"from-tag": "' + str(from_tag) + '",' if from_tag != '-1' else ''}
+        {'"address": "' + str(address) + '",' if address != '-1' else ''}
+        {'"label": "' + str(label) + '",' if label != '-1' else ''}
+        {'"flags": ["all"],' if all else ''}
+    }}
+    '''
+
+def stop_media(call_id, from_tag = '-1', address = '-1', label = '-1',
+                all = False):
+    ''' Stop playback. 
+
+    Stops the playback previously started by a play media message. Media
+    playback stops automatically when the end of the media file is
+    reached, so this message is only useful for prematurely stopping
+    playback.
+    '''
+
+    return f'''
+    {{
+        "command": "stop media",
+        "call-id": "{str(call_id)}"
+        {'"from-tag": "' + str(from_tag) + '",' if from_tag != '-1' else ''}
+        {'"address": "' + str(address) + '",' if address != '-1' else ''}
+        {'"label": "' + str(label) + '",' if label != '-1' else ''}
+        {'"flags": ["all"],' if all else ''}
+    }}
+    '''
+
+def play_dtmf(call_id, file, code, from_tag = '-1', address = '-1', 
+            label = '-1', all = False):
+    ''' Inject DTMF tone or event into a running audio stream.
+
+    The selected call participant is the one generating the DTMF event, 
+    not the one receiving it.
+    '''
+
+    # TODO: Add these: duration, volume, pause
+    return f'''
+    {{
+        "command": "play dtmf",
+        "call-id": "{str(call_id)}",
+        "code": "{str(code)}",
+        {'"from-tag": "' + str(from_tag) + '",' if from_tag != '-1' else ''}
+        {'"address": "' + str(address) + '",' if address != '-1' else ''}
+        {'"label": "' + str(label) + '",' if label != '-1' else ''}
+        {'"flags": ["all"],' if all else ''}
+    }}
+    '''
+
+def statistics():
+    ''' Returns a set of general statistics metrics. 
+    '''
+    return f'''
+    {{
+        "command": "statistics"
+    }}
+    '''
