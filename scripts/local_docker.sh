28983ad23fd1b25f62fbbb15317b0aa3ac98ac5f
Richárd Váradi <ricsi19981007@gmail.com>
2021-02-23 07:24:46 +0000

    Fix the script

--- scripts/local_docker.sh
+++ scripts/local_docker.sh
@@ -3,9 +3,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"/..
 
-registry=master:5000
-name=vidarhun/rtpe-operator
+registry=localhost:5000
+name=rtpe-operator
 tag=$registry/$name
 
-sudo docker build --no-cache  . -t $tag &&
-sudo docker push $tag:latest
+# POD=$(kubectl get pods --namespace kube-system -l k8s-app=registry \
+#            -o template --template '{{range .items}}{{.metadata.name}} {{.status.phase}}{{"\n"}}{{end}}' \
+#           | grep Running | head -1 | cut -f1 -d' ')
+
+# kubectl port-forward --namespace kube-system $POD 5000:5000 &
+
+sudo docker build --no-cache  . -t $tag --network=host &&
+sudo docker image push $tag
+
+
