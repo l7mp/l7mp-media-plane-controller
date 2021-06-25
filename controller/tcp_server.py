edbbb25c34b929b16848b042910bc254e5e127e6
Richárd Váradi <ricsi19981007@gmail.com>
2021-06-25 09:25:06 +0200

    Add retry and plus waiting time

--- controller/tcp_server.py
+++ controller/tcp_server.py
@@ -8,6 +8,7 @@ import random
 import string
 from utils import *
 from sockets import TCPSocket
+import time
 
 bc = bencodepy.Bencode(
     encoding='utf-8'
@@ -42,6 +43,7 @@ class TCPRequestHandler(socketserver.BaseRequestHandler):
                 if data['command'] == 'answer':
                     query = parse_bc(rtpe_socket.send(query_message(data['call-id'])))
                     create_resource(call_id, data['from-tag'], data['to-tag'], config, query)
+                time.sleep(0.05)
                 self.request.sendall(bytes(data['cookie'] + " " + bc.encode(response).decode(), 'utf-8'))
                 logging.debug("Response from rtpengine sent back to client")
         if config['sidecar_type'] == 'envoy':
